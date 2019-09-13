22a1ffef5738571af003571d7be91fb45921f571
Ubuntu <ubuntu@ip-172-31-24-253.ec2.internal>
2019-09-13 17:00:59 +0000

    added program

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Davin Rousseau
+# Created on September 2019
+# This proogram calculates perimeter and area of a rectangle
+# with dimensions 5cm and 3cm
+
+
+def main():
+    print("If a Rectangle has these dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
